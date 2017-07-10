982efd7b8de169dc4649a63fde429756f91dc0b9
Łukasz Wójcik <lukasz.wojcik@corp.smtsoftware.com>
2017-07-10 13:47:33 +0200

    use @connect decorator

--- src/containers/authorize/login.tsx
+++ src/containers/authorize/login.tsx
@@ -1,15 +1,41 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { connect } from 'react-redux';
+import { IAuthorizeLoginState } from './store';
+
+interface ILoginProps extends RouteComponentProps<any> {
+    login: string;
+    password: string;
+
+    changeLogin(login: string): void;
+    changePassword(password: string): void;
+}
 
 @connect((a, b) => {
-    console.log({a, b});
+    let { login } = a;
     return {
-        login: a.login
+        login: login.login,
+        password: login.password
     };
-})
-export default class LoginContainer extends React.Component<RouteComponentProps<any>, {}> {
+},
+    dispach => {
+        return {
+            changeLogin(login: string) {
+                dispach({ type: 'change_login', login });
+            },
+            changePassword(password: string) {
+                dispach({ type: 'change_password', password });
+            }
+        }
+    }
+)
+export default class LoginContainer extends React.Component<ILoginProps, {}> {
     render() {
-        return <div>Login</div>
+        return (
+            <div>
+                <input type="text" value={this.props.login} onChange={e => { this.props.changeLogin(e.target.value) }} />
+                <input type="password" value={this.props.password} onChange={e => { this.props.changePassword(e.target.value) }} />
+            </div>
+        );
     }
 }
--- src/containers/authorize/store/index.ts
+++ src/containers/authorize/store/index.ts
@@ -1,7 +1,18 @@
 import { createStore, combineReducers } from 'redux';
 
 export default createStore(combineReducers({
-    login: (s:string = '', a) => s,
-    password: (s:string = '', a) => s,
-    newPassword: (s:string = '', a) => s,
+    login: (s:IAuthorizeLoginState = { login: '', password: '' }, a) => {
+        switch(a.type) {
+            case 'change_login':
+                return Object.assign({}, s, { login: a.login }) ;
+            case 'change_password':
+                return Object.assign({}, s, { password: a.password }) ;
+        }
+        return s;
+    }
 }));
+
+export interface IAuthorizeLoginState {
+    login: string;
+    password: string;
+}
