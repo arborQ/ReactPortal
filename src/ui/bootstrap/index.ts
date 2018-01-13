d02229af352f3109cb1faf8e7fa520b76bce662b
Łukasz Wójcik <arbor@o2.pl>
2018-01-13 17:18:08 +0100

    switch to bootstrap

--- src/ui/bootstrap/index.ts
+++ src/ui/bootstrap/index.ts
@@ -1,10 +1,10 @@
-import CardComponent from "../material/card";
 import FormComponent from "../material/form";
 import GridComponent from "../material/grid";
 import HeaderComponent from "../material/header";
 import LoadingComponent from "../material/loader";
 import Styles from "../material/styles";
 import ButtonComponent from "./button";
+import CardComponent from "./card";
 import InputComponent from "./input";
 
 export {
--- src/ui/material/index.ts
+++ src/ui/material/index.ts
@@ -1,12 +1,9 @@
-import ButtonComponent from "../bootstrap/button";
-// import ButtonComponent from "./button";
-// import CardComponent from "./card";
-import CardComponent from "../bootstrap/card";
-// import InputComponent from "./input";
-import InputComponent from "../bootstrap/input";
+import ButtonComponent from "./button";
+import CardComponent from "./card";
 import FormComponent from "./form";
 import GridComponent from "./grid";
 import HeaderComponent from "./header";
+import InputComponent from "./input";
 import LoadingComponent from "./loader";
 
 import Styles from "./styles";
