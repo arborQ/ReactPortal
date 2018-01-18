bb0b4d5585eb8cf91b83dcdd39763a642bd03c4f
Łukasz Wójcik <arbor@o2.pl>
2018-01-18 21:56:36 +0100

    go back

--- src/ui/material/dialog/dialog.elements.ts
+++ src/ui/material/dialog/dialog.elements.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Dialog = styled.dialog`
     z-index: 1000;
     border: none;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);
 `;
 
 export const Backdrop = styled.div`
@@ -11,6 +12,7 @@ export const Backdrop = styled.div`
     bottom: 0;
     left: 0;
     right: 0;
-    background: rgba(0,0,0,0.1);
+    background: rgba(0,0,0,0.2);
     z-index: 999;
+    filter: blur(5px);
 `;
