910e32c1e0999ddd8b1467e61dcdb88e63030066
Łukasz Wójcik <arbor@o2.pl>
2018-03-18 20:27:17 +0100

    webpack4

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
-import { createBrowserHistory } from "history";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { Route, Router, Switch } from "react-router";
-import { BrowserRouter } from "react-router-dom";
-import Home from "./containers/home";
+// import { createBrowserHistory } from "history";
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+// import { Provider } from "react-redux";
+// import { Route, Router, Switch } from "react-router";
+// import { BrowserRouter } from "react-router-dom";
+// import Home from "./containers/home";
 
-const history = createBrowserHistory();
+// const history = createBrowserHistory();
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <Home />
+//   </BrowserRouter>,
+//   document.getElementById("root"),
+// );
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Home />
-  </BrowserRouter>,
+  <div>WORKS!!!!</div>,
   document.getElementById("root"),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,17 @@
-var webpack = require('webpack');
-var path = require('path');
+// webpack.config.js
 
-// variables
-var isProduction = process.argv.indexOf('-p') >= 0;
-var sourcePath = path.join(__dirname, './src');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+var webpack = require('webpack'); //to access built-in plugins
+var path = require('path');
 var outPath = path.join(__dirname, './dist');
 
-// plugins
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-module.exports = {
-  context: sourcePath,
-  entry: {
+var config = {
+    entry: {
     main: [
       "bx-utils",
       "bx-ui",      
-      './index.tsx'],
-
+      './src/index.tsx'
+    ],
     vendor: [
       'react',
       'react-dom',
@@ -37,120 +31,87 @@ module.exports = {
     chunkFilename: '[id].chunk.js?_=[chunkhash]'
 
   },
-  target: 'web',
   resolve: {
     extensions: ['.js', '.ts', '.tsx'],
-    // Fix webpack's default behavior to not load packages with jsnext:main module
-    // https://github.com/Microsoft/TypeScript/issues/11677
     mainFields: ['main'],
     alias: {
       'bx-ui': path.resolve(__dirname, './src/ui/material'),
       "bx-utils": path.resolve(__dirname, './src/utils')
     }
   },
+  mode: 'development',
   module: {
-    loaders: [{
-        test: /\.ts$/,
-        enforce: 'pre',
-        loader: 'tslint-loader',
-        exclude: [/node_modules/],
-        options: { /* Loader options go here */ }
-      },
-      // .ts, .tsx
+    rules: [
       {
         test: /\.tsx?$/,
-        use: isProduction ?
-          'awesome-typescript-loader?module=es6' :
-          [
-            'awesome-typescript-loader'
-          ]
-      },
-      // css
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [{
-              loader: 'css-loader',
-              query: {
-                modules: true,
-                sourceMap: !isProduction,
-                importLoaders: 1,
-                localIdentName: '[local]__[hash:base64:5]'
-              }
-            },
-            {
-              loader: 'postcss-loader'
-            }
-          ]
-        })
-      },
-      // static assets
-      {
-        test: /\.html$/,
-        use: 'html-loader'
-      },
-      {
-        test: /\.png$/,
-        use: 'url-loader?limit=10000'
-      },
-      {
-        test: /\.jpg$/,
-        use: 'file-loader'
-      },
-    ],
-  },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        context: sourcePath,
-        postcss: [
-          require('postcss-import')({
-            addDependencyTo: webpack
-          }),
-          require('postcss-url')(),
-          require('postcss-cssnext')(),
-          require('postcss-reporter')(),
-          require('postcss-browser-reporter')({
-            disabled: isProduction
-          }),
-        ]
+        use: 'awesome-typescript-loader'
       }
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: 'vendor.bundle.js',
-      minChunks: Infinity
-    }),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    // new webpack.LoaderOptionsPlugin({
-    //     minimize: true,
-    //     debug: false
-    // }),
-    // new webpack.DefinePlugin({
-    //   'process.env': {
-    //     NODE_ENV: JSON.stringify('production')
-    //   }
-    // }),
-    // new webpack.optimize.UglifyJsPlugin({ output: {comments: false} }),
-    new ExtractTextPlugin({
-      filename: 'styles.css',
-      disable: !isProduction
-    }),
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    })
-  ],
-  devServer: {
-    contentBase: sourcePath,
-    stats: {
-      warnings: false
-    },
+    ]
   },
-  node: {
-    // workaround for webpack-dev-server issue
-    // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
-    fs: 'empty',
-    net: 'empty'
-  }
 };
+
+module.exports = config;
+
+// var webpack = require('webpack');
+// var path = require('path');
+
+// // variables
+// var isProduction = process.argv.indexOf('-p') >= 0;
+// var sourcePath = path.join(__dirname, './src');
+// var outPath = path.join(__dirname, './dist');
+
+// // plugins
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// module.exports = {
+//   context: sourcePath,
+//   entry: {
+//     main: [
+//       "bx-utils",
+//       "bx-ui",      
+//       './index.tsx'],
+
+//     vendor: [
+//       'react',
+//       'react-dom',
+//       'react-redux',
+//       'react-router',
+//       'redux',
+//       'styled-components',
+//     ],
+//     polifil: [ 
+//       "dialog-polyfill",
+//     ]
+//   },
+//   output: {
+//     path: outPath,
+//     publicPath: '/',
+//     filename: '[name].bundle.js?_=[chunkhash]',
+//     chunkFilename: '[id].chunk.js?_=[chunkhash]'
+
+//   },
+//   target: 'web',
+//   resolve: {
+//     extensions: ['.js', '.ts', '.tsx'],
+//     // Fix webpack's default behavior to not load packages with jsnext:main module
+//     // https://github.com/Microsoft/TypeScript/issues/11677
+//     mainFields: ['main'],
+//     alias: {
+//       'bx-ui': path.resolve(__dirname, './src/ui/material'),
+//       "bx-utils": path.resolve(__dirname, './src/utils')
+//     }
+//   },
+//   devServer: {
+//     contentBase: sourcePath,
+//     stats: {
+//       warnings: false
+//     },
+//   },
+//   node: {
+//     // workaround for webpack-dev-server issue
+//     // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
+//     fs: 'empty',
+//     net: 'empty'
+//   }
+// };
