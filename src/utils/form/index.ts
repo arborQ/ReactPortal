0abbb4710cdd6d8dc3dac7ef538f992c700a89de
Łukasz Wójcik <arbor@o2.pl>
2018-03-25 23:07:09 +0200

    forms

--- src/utils/form/index.ts
+++ src/utils/form/index.ts
@@ -1,13 +1,42 @@
-export class Form<TModel> {
-  getModel(): TModel {
-    return {} as TModel;
-  }
+type stateChangeAction<T> = (model: T | null) => Promise<T> | undefined;
 
-  get isValid(): boolean {
-    return true;
+interface IControlDictionary<T extends IControl<any>> {
+  [key: string]: T;
+}
+
+interface IControl<T> {
+  state: T | null;
+  changeState(model: T | null): void;
+  subscribe(action: stateChangeAction<T>): void;
+}
+
+interface IRootControl<T> extends IControl<T> {
+  childControls: IControlDictionary<any>;
+  addControl<TInnerControl extends IControl<TInnerControl>>(name: string, control: TInnerControl): void;
+}
+
+class Control<TModel> implements IControl<TModel> {
+  state: TModel | null = null;
+  private subscribers: Array<stateChangeAction<TModel>> = [];
+  changeState(model: TModel | null): void {
+    this.state = model;
+
+    this.subscribers.forEach((item) => item(this.state));
+  }
+  subscribe(action: (model: TModel) => Promise<TModel> | undefined): void {
+    this.subscribers.push(action);
   }
 }
 
-interface IFormControl<T> {
-  onChange: (value: T) => void;
+class RootControl<TModel>
+  extends Control<TModel>
+  implements IRootControl<TModel> {
+  childControls: IControlDictionary<any> = {};
+  addControl<TInnerControl extends IControl<TInnerControl>>(name: string, control: TInnerControl): void {
+    this.childControls[name] = control;
+  }
+}
+
+export class Form<TModel> implements RootControl<TModel> {
+
 }
