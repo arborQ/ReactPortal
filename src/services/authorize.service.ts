4cb8a09eaa6ccbb0cee8136a189e436fb0582be9
Lukasz Wojcik <arbor@o2.pl>
2018-05-08 09:15:14 +0200

    fix lol

--- src/services/authorize.service.ts
+++ src/services/authorize.service.ts
@@ -2,20 +2,22 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 
 export default class AuthorizeService {
-    constructor(private firebaseApp: firebase.app.App) {
+    private auth: firebase.auth.Auth;
 
+    constructor(firebaseApp: firebase.app.App) {
+        this.auth = firebase.auth();
     }
 
     login(login: string, password: string): Promise<void> {
-        return this.firebaseApp.auth().signInWithEmailAndPassword(login, password);
+        return this.auth.signInWithEmailAndPassword(login, password);
     }
 
     logout(): Promise<void> {
-        return this.firebaseApp.auth().signOut();
+        return this.auth.signOut();
     }
 
     statusChanged(action: (currentUser: Services.Authorize.IUser | null) => void): void {
-        this.firebaseApp.auth().onAuthStateChanged((user: any) => {
+        this.auth.onAuthStateChanged((user: any) => {
             if (!!user) {
                 const bxUser: Services.Authorize.IUser = {
                     email: user.email, login: user.displayName, uid: user.uid,
--- src/services/temperature.service.ts
+++ src/services/temperature.service.ts
@@ -1,23 +1,21 @@
 import * as firebase from "firebase/app";
-// import "firebase/firestore";
+// import "firebase/database";
 
 export default class UsersService {
-  constructor(private firebaseApp: firebase.app.App) {
+  private collection: firebase.database.Reference;
 
+  constructor(firebaseApp: firebase.app.App) {
+    // this.collection = firebase.database().ref("temperature");
   }
+
+  // add(newRecord: Services.Temperature.ITemperature): Promise<any> {
+  //   return this.collection.
+  // }
 
   list(): Promise<any[]> {
-    return Promise.resolve([]);
-    // return firebase.firestore().collection("temperature").get().then(snap => snap.forEach(;
-    // const items = firebase.database().ref("temperature").orderByChild("date");
-
-    // return items.once("value").then((snapshot) => {
-    //   console.log("snap", snapshot);
-    //   return snapshot.val();
-    // });
-  }
-
-  add(newRecord: Services.Temperature.ITemperature): Promise<Services.Temperature.ITemperature> {
-    return Promise.resolve(newRecord);
+    // firebase.database().ref("temperature");
+    return new Promise<any[]>((resolve) => {
+      this.collection.limitToFirst(50);
+    });
   }
 }
