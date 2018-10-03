9665961305367584128dce04ab664a66ef6a9a0c
Lukasz Wojcik <lwojcik@pgs-soft.com>
2018-10-03 10:55:53 +0200

    proxy server

---
+++ bin/proxy.server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 8080;
+
+app.use(express.static('public'));;
+
+
+app.listen(port, () => console.log(`Proxy listening on port ${port}!`))
