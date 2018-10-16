c51bc0fa12aae8a4000bd2b02ef40554052a6358
Lukasz Wojcik <lwojcik@pgs-soft.com>
2018-10-16 13:49:02 +0200

    to function

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static('public'));;
+app.use(express.static("public"));
 
-
-app.listen(port, () => console.log(`Proxy listening on http://localhost:${port}!`))
+app.listen(port, function() {
+  console.log("Proxy listening on http://localhost:" + port);
+});
